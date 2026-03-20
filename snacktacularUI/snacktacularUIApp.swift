ee9b95424a2fe6aef6ad45e618b55c8c8164c2eb
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-03-20 13:19:50 -0400

    Initial Commit

---
+++ snacktacularUI/snacktacularUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  snacktacularUIApp.swift
+//  snacktacularUI
+//
+//  Created by Nia Mitchell on 3/20/26.
+//
+
+import SwiftUI
+
+@main
+struct snacktacularUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
